a9b2307b01cc57e95289d6be2a6f19b2e587f942
ahmed5100fa <ahmed5722144@gmail.com>
2025-11-16 14:46:06 +0200

    auth first  editing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
-import { AuthLayout } from './features/pages/auth/pages/auth-layout/auth-layout';
-import { Signin } from './features/pages/auth/pages/signin/signin';
-import { Signup } from './features/pages/auth/pages/signup/signup';
-import { ForgetPassword } from './features/pages/auth/pages/forget-password/forget-password';
-import { VerfiyCode } from './features/pages/auth/pages/verfiy-code/verfiy-code';
-import { ChangePassword } from './features/pages/auth/pages/change-password/change-password';
+import { ForgetPassword } from './features/auth/pages/forget-password/forget-password';
+import { AuthLayout } from './features/auth/pages/layout/auth-layout/auth-layout';
+import { Signin } from './features/auth/pages/signin/signin';
+import { Signup } from './features/auth/pages/signup/signup';
 
 export const routes: Routes = [
     {path : '' , component : AuthLayout , children : [
         {path : '' , redirectTo : 'login' , pathMatch : "full"},
         {path : 'login' , component: Signin , title : "signIn"},
         {path : 'signup' , component: Signup , title : "signUp"},
-        {path : 'forgetpassword' , component : ForgetPassword , title : "forgetPassword"},
-        {path : 'verifycode' , component : VerfiyCode , title : "verifyCode"},
-        {path : 'changepassword' , component : ChangePassword , title : "changePassword"}
+        {path : 'forgetpassword' , component : ForgetPassword , title : "forgetPassword"}
     ]}
 ];
---
+++ src/app/features/auth/pages/signup/signup.ts
@@ -0,0 +1,16 @@
+import { Component, ElementRef, ViewChild, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [RouterLink ],
+  templateUrl: './signup.html',
+  styleUrls: ['./signup.scss']
+})
+export class Signup {
+
+
+
+
+}
